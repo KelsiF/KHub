bf0f1e34393252e707a15ab3830a777f1b9b3bab
Kelsi <superigromen2018@gmail.com>
2023-07-10 15:41:26 +0300

    1.1

--- src/main/java/org/kelsidev/khub.java
+++ src/main/java/org/kelsidev/khub.java
@@ -1,11 +1,21 @@
 package org.kelsidev;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kelsidev.commands.hubCommand;
 import org.kelsidev.commands.sethubCommand;
 
-public final class khub extends JavaPlugin {
+import java.util.Objects;
 
+
+
+public final class khub extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -16,8 +26,32 @@ public final class khub extends JavaPlugin {
         getCommand("hub").setExecutor(new hubCommand(this));
     }
 
+
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static boolean PAPI;
+
+    public static String setPlaceholders(String text, Player player) {
+        if (text.contains("%player%") && player != null) {
+            text = text.replace("%player%", player.getName());
+        }
+
+        if (PAPI && player != null) {
+            text = PlaceholderAPI.setPlaceholders(player, text);
+        }
+
+        return text;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.sendMessage(getConfig().getString("messages.player-join"));
+        }
+    }
+
 }
